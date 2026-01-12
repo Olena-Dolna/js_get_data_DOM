956caa75d7efd5424f9fd6683d3fddf5ca28ec68
Olena Dolna <dolnaolena2019@gmail.com>
2026-01-12 22:06:35 +0100

    solution added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-// write your code here
+const populationNodes = document.querySelectorAll('.population');
+
+let total = 0;
+
+for (const populationNode of populationNodes) {
+  let population = '';
+
+  for (const symbol of populationNode.textContent) {
+    if (symbol === ',') {
+      continue;
+    }
+
+    population += symbol;
+  }
+
+  total += +population;
+}
+
+const average = Math.round(total / populationNodes.length);
+
+const totalFormatted = getFormattedValue(String(total));
+const averageFormatted = getFormattedValue(String(average));
+
+const totalValue = document.querySelector('.total-population');
+const averageValue = document.querySelector('.average-population');
+
+totalValue.textContent = totalFormatted;
+averageValue.textContent = averageFormatted;
+
+function getFormattedValue(value) {
+  let result = '';
+
+  for (let i = value.length - 1; i >= 0; i--) {
+    if (i !== value.length - 1 && (value.length - 1 - i) % 3 === 0) {
+      result = ',' + result;
+    }
+
+    result = value[i] + result;
+  }
+
+  return result;
+}
